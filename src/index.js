ea1642744a042df8f084c59bf09b6a50990172aa
Damon <damonbutler1994@gmail.com>
2023-02-07 11:04:07 -0500

    Damon changes

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
 let toyContainer = document.querySelector("#toy-collection");
 
+fetch("http://localhost:3000/toys")
+.then(res => res.json())
+.then(data => {
+  renderToyCards(data);
+})
+function renderToyCards(toys) {
+  // let divCard = document.createElement("div")
+  toys.forEach(createElement("div"))
+}
+
+
+// divCard.setAttribute("class", "card")
+// toyContainer.append(divCard);
+
 // let h2 = document.createElement("h2")
+// // h2.innerText = toys.name
+
 // let img = document.createElement("img")
+// img.setAttribute()
+
 // let p = document.createElement("p")
+
+
 // let btn = document.createElement("btn")
 
 
-
-
-
-
-
-function getToy () {
-  const divCard = document.createElement("div")
-  // divCard.setAttribute("class", "card")
-  // document.append(divCard);
-  // divCard.append(h2, img, p, btn);
-
-  fetch("http://localhost:3000/toys")
-    .then(res => res.json())
-    .then(data => divCard.innerText = data)
-
-
-}
-
-getToy();
-
-
-
-
-
+// divCard.append(h2, img, p, btn);
 
 
 
