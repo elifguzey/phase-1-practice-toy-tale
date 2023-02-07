3bedf79bc43843f41d98062b0cd881dc927c4b92
Kevin Salinas <kevinsalinas155@gmail.com>
2023-02-07 10:20:42 -0500

    kevin updates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+let toyContainer = document.querySelector("#toy-collection");
+
+// let h2 = document.createElement("h2")
+// let img = document.createElement("img")
+// let p = document.createElement("p")
+// let btn = document.createElement("btn")
+
+
+
+
+
+
+
+function getToy () {
+  const divCard = document.createElement("div")
+  // divCard.setAttribute("class", "card")
+  // document.append(divCard);
+  // divCard.append(h2, img, p, btn);
+
+  fetch("http://localhost:3000/toys")
+    .then(res => res.json())
+    .then(data => divCard.innerText = data)
+
+
+}
+
+getToy();
+
+
+
+
+
+
+
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,4 +48,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-hiiiii
+
+
+
